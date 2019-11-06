60423223c20b5321bc461e5b7489001fdf4232b5
Dennis Leon <dleon@pivotal.io>
2019-11-06 10:53:47 -0800

    Start a http server listening on a hardcoded port
    [#169576558](https://www.pivotaltracker.com/story/show/169576558)
    
    Authored-by: Dennis Leon <dleon@pivotal.io>

--- src/smb-broker/main.go
+++ src/smb-broker/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	var errChan = make(chan error)
+	go func() {
+		err := http.ListenAndServe("localhost:8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := w.Write([]byte("Hi"))
+			if err != nil {
+				panic(err)
+			}
+		}))
+		errChan <- err
+	}()
 	fmt.Println("Started")
+	<-errChan
 }
--- src/smb-broker/main_test.go
+++ src/smb-broker/main_test.go
@@ -5,15 +5,34 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
+	"io/ioutil"
+	"net/http"
 	"os/exec"
 )
 
 var _ = Describe("Main", func() {
+	var session *gexec.Session
+
 	JustBeforeEach(func() {
 		smbBrokerCmd := exec.Command(smbBrokerCompiledPath)
-		session, err := gexec.Start(smbBrokerCmd, GinkgoWriter, GinkgoWriter)
+		var err error
+		session, err = gexec.Start(smbBrokerCmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 		Eventually(session).Should(gbytes.Say("Started"))
 	})
 
+	AfterEach(func() {
+		session.Kill()
+	})
+
+	It("should list catalog of services offered by the SMB service broker", func() {
+		resp, err := http.DefaultClient.Get("http://localhost:8080/v2/catalog")
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(resp.Status).To(Equal("200 OK"))
+
+		bytes, err := ioutil.ReadAll(resp.Body)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(bytes).Should(ContainSubstring("Hi"))
+	})
 })
