46e7c55b81ca802845b10e535dfa3c8bc5356e5e
Dennis Leon <dleon@pivotal.io>
2019-11-14 16:14:32 -0800

    Test refactor
    Authored-by: Dennis Leon <dleon@pivotal.io>

--- smb-broker/handlers_test.go
+++ smb-broker/handlers_test.go
@@ -12,50 +12,53 @@ import (
 )
 
 var _ = Describe("Handlers", func() {
+	var recorder *httptest.ResponseRecorder
+	var request *http.Request
+	var store *storefakes.FakeServiceInstanceStore
+
+	BeforeEach(func() {
+		recorder = httptest.NewRecorder()
+		store = &storefakes.FakeServiceInstanceStore{}
+	})
+
+	JustBeforeEach(func() {
+		BrokerHandler(store).ServeHTTP(recorder, request)
+	})
 
 	Describe("#Catalog endpoint", func() {
-		makeCatalogRequest := func() *httptest.ResponseRecorder {
-			recorder := httptest.NewRecorder()
-			request, _ := http.NewRequest(http.MethodGet, "/v2/catalog", nil)
-
-			BrokerHandler(nil).ServeHTTP(recorder, request)
-			return recorder
-		}
+		BeforeEach(func() {
+			var err error
+			request, err = http.NewRequest(http.MethodGet, "/v2/catalog", nil)
+			Expect(err).NotTo(HaveOccurred())
+		})
 
 		It("should list catalog of services offered by the SMB service broker", func() {
-			response := makeCatalogRequest()
-			Expect(response.Code).To(Equal(200))
-			Expect(response.Body).To(MatchJSON(fixture("catalog.json")))
+			Expect(recorder.Code).To(Equal(200))
+			Expect(recorder.Body).To(MatchJSON(fixture("catalog.json")))
 		})
 	})
 
 	Describe("#Provision endpoint", func() {
-		makeProvisionRequest := func() *httptest.ResponseRecorder {
-			recorder := httptest.NewRecorder()
-			request, _ := http.NewRequest(http.MethodPut, "/v2/service_instances/123", strings.NewReader(`{ "service_id": "123", "plan_id": "plan-id" }`))
-
-			BrokerHandler(nil).ServeHTTP(recorder, request)
-			return recorder
-		}
+		BeforeEach(func() {
+			var err error
+			request, err = http.NewRequest(http.MethodPut, "/v2/service_instances/123", strings.NewReader(`{ "service_id": "123", "plan_id": "plan-id" }`))
+			Expect(err).NotTo(HaveOccurred())
+		})
 
 		It("should allow provisioning a new service", func() {
-			response := makeProvisionRequest()
-			Expect(response.Code).To(Equal(201))
-			Expect(response.Body).To(MatchJSON(`{}`))
+			Expect(recorder.Code).To(Equal(201))
+			Expect(recorder.Body).To(MatchJSON(`{}`))
 		})
 	})
 
 	Describe("#GetInstance endpoint", func() {
-		var store = &storefakes.FakeServiceInstanceStore{}
-		makeProvisionRequest := func() *httptest.ResponseRecorder {
-			recorder := httptest.NewRecorder()
-			request, err := http.NewRequest(http.MethodGet, "/v2/service_instances/123", nil)
+		BeforeEach(func() {
+			var err error
+			request, err = http.NewRequest(http.MethodGet, "/v2/service_instances/123", nil)
 			Expect(err).NotTo(HaveOccurred())
 			request.Header.Add("X-Broker-API-Version","2.14")
+		})
 
-			BrokerHandler(store).ServeHTTP(recorder, request)
-			return recorder
-		}
 
 		BeforeEach(func() {
 			store.GetReturns(map[string]interface{} {
@@ -64,9 +67,8 @@ var _ = Describe("Handlers", func() {
 		})
 
 		It("should allow provisioning a new service", func() {
-			response := makeProvisionRequest()
-			Expect(response.Code).To(Equal(200))
-			Expect(response.Body).To(MatchJSON(`{ "service_id": "", "plan_id": "", "parameters": { "key1": "val1" } }`))
+			Expect(recorder.Code).To(Equal(200))
+			Expect(recorder.Body).To(MatchJSON(`{ "service_id": "", "plan_id": "", "parameters": { "key1": "val1" } }`))
 		})
 	})
 })
