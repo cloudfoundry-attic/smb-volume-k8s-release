dd3b517c10fd4de564a374dd05d47357955cb424
Dennis Leon <dleon@pivotal.io>
2020-01-06 11:39:49 -0800

    Add scripts to build a docker image that can run docker (that is configured to run kind)
    [#169169378](https://www.pivotaltracker.com/story/show/169169378)
    
    Authored-by: Dennis Leon <dleon@pivotal.io>

--- smb-broker/ci/unit-tests.sh
+++ smb-broker/ci/unit-tests.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
-set -e
+set -ex
+
+export PATH=~/go/bin:$PATH
+
+mkdir -p /etc/docker
+cat <<EOF >/etc/docker/daemon.json
+{
+  "storage-driver": "aufs"
+}
+EOF
+
+/usr/local/bin/start-docker &
+
+rc=1
+for i in $(seq 1 100); do
+  echo waiting for docker to come up...
+  set +e
+  docker info
+  rc=$?
+  set -e
+  if [ "$rc" -eq "0" ]; then
+      break
+  fi
+  sleep 1
+done
+
+if [ "$rc" -ne "0" ]; then
+  exit 1
+fi
 
 make --directory=smb-volume-k8s-release/smb-broker test
+pkill dockerd
