7ac529336569c56dad89f5f9419d0c53f8b7dc9c
Dennis Leon <dleon@pivotal.io>
2020-01-24 12:05:03 -0800

    Vendor in the csi-test repo and invoke it from our test suite
    [#170690947](https://www.pivotaltracker.com/story/show/170690947)
    
    Co-authored-by: Dennis Leon <dleon@pivotal.io>
    Co-authored-by: Paul Warren <pwarren@pivotal.io>

--- smb-csi-driver/go.mod
+++ smb-csi-driver/go.mod
@@ -3,6 +3,7 @@ module code.cloudfoundry.org/smb-csi-driver
 go 1.13
 
 require (
+	github.com/kubernetes-csi/csi-test/v3 v3.0.0
 	github.com/onsi/ginkgo v1.11.0
 	github.com/onsi/gomega v1.8.1
 )
--- smb-csi-driver/smb_csi_driver_suite_test.go
+++ smb-csi-driver/smb_csi_driver_suite_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"testing"
 
+	"github.com/kubernetes-csi/csi-test/v3/pkg/sanity"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -10,4 +11,11 @@ import (
 func TestSmbCsiDriver(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "SmbCsiDriver Suite")
+
+	config := sanity.NewTestConfig()
+	// Set configuration options as needed
+	config.Address = ""
+
+	// Now call the test suite
+	sanity.Test(t, config)
 }
