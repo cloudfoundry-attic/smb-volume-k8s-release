e988eedcc99c7e01799ee97f2924796e4faf4897
Dennis Leon <dleon@pivotal.io>
2020-01-24 14:39:27 -0800

    Test code
    - build the csi identity server and run it before running csi identity server tests
    
    [#170690947](https://www.pivotaltracker.com/story/show/170690947)
    
    Co-authored-by: Dennis Leon <dleon@pivotal.io>
    Co-authored-by: Paul Warren <pwarren@pivotal.io>

--- smb-csi-driver/go.mod
+++ smb-csi-driver/go.mod
@@ -3,7 +3,9 @@ module code.cloudfoundry.org/smb-csi-driver
 go 1.13
 
 require (
+	github.com/container-storage-interface/spec v1.2.0
 	github.com/kubernetes-csi/csi-test/v3 v3.0.0
 	github.com/onsi/ginkgo v1.11.0
 	github.com/onsi/gomega v1.8.1
+	google.golang.org/grpc v1.25.1
 )
--- smb-csi-driver/smb_csi_driver_suite_test.go
+++ smb-csi-driver/smb_csi_driver_suite_test.go
@@ -1,13 +1,31 @@
 package main_test
 
 import (
+	"github.com/onsi/gomega/gexec"
+	"os/exec"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
+var smbCsiDriverPath string
+var session *gexec.Session
 
 func TestSmbCsiDriver(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "SmbCsiDriver Suite")
 }
+
+var _ = BeforeSuite(func() {
+	var err error
+	smbCsiDriverPath, err = gexec.Build("code.cloudfoundry.org/smb-csi-driver", "-mod", "vendor")
+	Expect(err).NotTo(HaveOccurred())
+
+	cmd := exec.Command(smbCsiDriverPath)
+	session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	Expect(session.Kill().Wait().ExitCode()).To(Equal(137))
+})
