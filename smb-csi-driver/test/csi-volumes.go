3c877846d22a6906727df1a43d450ba2c09c3015
Paul Warren <pwarren@pivotal.io>
2020-02-07 14:30:32 -0800

    Make e2e test cleanup after itself
    [#170938410](https://www.pivotaltracker.com/story/show/170938410)
    
    Co-authored-by: Paul Warren <pwarren@pivotal.io>
    Co-authored-by: Lisa Cho <lcho@pivotal.io>

--- smb-csi-driver/test/csi-volumes.go
+++ smb-csi-driver/test/csi-volumes.go
@@ -25,15 +25,16 @@ var _ = utils.SIGDescribe("CSI Volumes", func() {
 
 })
 
-type noopTestDriver struct {}
+type noopTestDriver struct{}
 
 type smbVolume struct {
 	serverIP  string
 	serverPod *v1.Pod
-	f         *framework.Framework
+	framework *framework.Framework
+	config    volume.TestConfig
 }
 
-var _ testsuites.TestVolume = &smbVolume{}
+var _ testsuites.TestDriver = &noopTestDriver{}
 var _ testsuites.PreprovisionedVolumeTestDriver = &noopTestDriver{}
 var _ testsuites.PreprovisionedPVTestDriver = &noopTestDriver{}
 
@@ -41,7 +42,7 @@ func (n noopTestDriver) GetPersistentVolumeSource(readOnly bool, fsType string, 
 	vol, _ := testVolume.(*smbVolume)
 	return &v1.PersistentVolumeSource{
 		CSI: &v1.CSIPersistentVolumeSource{
-			Driver: "org.cloudfoundry.smb",
+			Driver:       "org.cloudfoundry.smb",
 			VolumeHandle: "volume-handle",
 			VolumeAttributes: map[string]string{
 				"server":   vol.serverIP,
@@ -68,13 +69,11 @@ func (n noopTestDriver) SkipUnsupportedTest(pattern testpatterns.TestPattern) {
 	}
 }
 
-
 func (n noopTestDriver) PrepareTest(f *framework.Framework) (*testsuites.PerTestConfig, func()) {
 	return &testsuites.PerTestConfig{
 		Driver:    n,
 		Prefix:    "smb",
 		Framework: f,
-
 	}, nil
 }
 
@@ -84,24 +83,25 @@ func (n noopTestDriver) CreateVolume(config *testsuites.PerTestConfig, volType t
 	ns := f.Namespace
 
 	serverConfig := volume.TestConfig{
-		Namespace: ns.Name,
-		Prefix:   "smb",
-		ServerImage: "dperson/samba",
-		ServerPorts: []int{139,445},
-		ServerArgs: []string{"-p", "-u", "example1;badpass", "-s", "example1;/example1;no;no;no;example1", "-p", "-S"},
-		ServerVolumes: map[string]string{},
+		Namespace:          ns.Name,
+		Prefix:             "smb",
+		ServerImage:        "dperson/samba",
+		ServerPorts:        []int{139, 445},
+		ServerArgs:         []string{"-p", "-u", "example1;badpass", "-s", "example1;/example1;no;no;no;example1", "-p", "-S"},
+		ServerVolumes:      map[string]string{},
 		ServerReadyMessage: "finished starting up",
 	}
 
 	serverPod, serverIP := volume.CreateStorageServer(cs, serverConfig)
+
 	return &smbVolume{
-		serverIP: serverIP,
+		serverIP:  serverIP,
 		serverPod: serverPod,
-		f: f,
+		framework: f,
+		config:    serverConfig,
 	}
 }
 
-
-func (v smbVolume) DeleteVolume(){
-
+func (v *smbVolume) DeleteVolume() {
+	volume.TestCleanup(v.framework, v.config)
 }
