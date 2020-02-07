03c91e878941b11c7ace1c551b63341929fbf4c5
Lisa Cho <lcho@pivotal.io>
2020-02-06 16:41:23 -0800

    Spin up a samba server during e2e tests
    Authored-by: Lisa Cho <lcho@pivotal.io>

--- smb-csi-driver/test/csi-volumes.go
+++ smb-csi-driver/test/csi-volumes.go
@@ -6,6 +6,7 @@ import (
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/kubernetes/test/e2e/framework"
+	"k8s.io/kubernetes/test/e2e/framework/volume"
 	"k8s.io/kubernetes/test/e2e/storage/testpatterns"
 	"k8s.io/kubernetes/test/e2e/storage/testsuites"
 	"k8s.io/kubernetes/test/e2e/storage/utils"
@@ -30,10 +31,16 @@ var _ testsuites.PreprovisionedVolumeTestDriver = &noopTestDriver{}
 var _ testsuites.PreprovisionedPVTestDriver = &noopTestDriver{}
 
 func (d noopTestDriver) GetPersistentVolumeSource(readOnly bool, fsType string, testVolume testsuites.TestVolume) (*v1.PersistentVolumeSource, *v1.VolumeNodeAffinity) {
+	vol, _ := testVolume.(*smbVolume)
 	return &v1.PersistentVolumeSource{
 		CSI: &v1.CSIPersistentVolumeSource{
 			Driver: "org.cloudfoundry.smb",
 			VolumeHandle: "volume-handle",
+			VolumeAttributes: map[string]string{
+				"server":   vol.serverIP,
+				"share":    "/example1",
+				"readOnly": "true",
+			},
 		},
 	}, nil
 }
@@ -59,7 +66,35 @@ func (n noopTestDriver) PrepareTest(f *framework.Framework) (*testsuites.PerTest
 }
 
 func (d noopTestDriver) CreateVolume(config *testsuites.PerTestConfig, volType testpatterns.TestVolType) testsuites.TestVolume {
-	return d
+	f := config.Framework
+	cs := f.ClientSet
+	ns := f.Namespace
+
+	serverConfig := volume.TestConfig{
+		Namespace: ns.Name,
+		Prefix:   "smb",
+		ServerImage: "dperson/samba",
+		ServerPorts: []int{139,445},
+		ServerArgs: []string{"-p", "-u", "example1;badpass", "-s", "example1;/example1;no;no;no;example1", "-p", "-S"},
+		ServerVolumes: map[string]string{},
+		ServerReadyMessage: "finished starting up",
+	}
+
+	serverPod, serverIP := volume.CreateStorageServer(cs, serverConfig)
+	return &smbVolume{
+		serverIP: serverIP,
+		serverPod: serverPod,
+		f: f,
+	}
+}
+
+type smbVolume struct {
+	serverIP  string
+	serverPod *v1.Pod
+	f         *framework.Framework
+}
+func (smbVolume) DeleteVolume(){
+
 }
 
 func (noopTestDriver) DeleteVolume() {
