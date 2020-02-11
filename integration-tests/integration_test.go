3390574dd7ebf148656f7121cdd36c661cee125e
Dennis Leon <dleon@pivotal.io>
2020-02-10 16:00:51 -0800

    Test refactor
    - make the instance id and expected written contents wire into the test via variables
    
    [#167875608](https://www.pivotaltracker.com/story/show/167875608)
    
    Co-authored-by: Dennis Leon <dleon@pivotal.io>
    Co-authored-by: Lisa Cho <lcho@pivotal.io>

--- integration-tests/integration_test.go
+++ integration-tests/integration_test.go
@@ -5,18 +5,22 @@ import (
 	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
+	"text/template"
 )
 
 var _ = Describe("Integration", func() {
-	var output string
+	var fileReadContents string
+	var expectedFileContents = "hi"
 
 	BeforeEach(func() {
-		var podIP string
+		var podIP, username, password string
 
 		By("deploying a smb server", func() {
-			overrides := `{"spec": {"template":  {"spec": {"containers": [{"name": "test-smb1", "command": [ "/sbin/tini", "--", "/usr/bin/samba.sh", "-p","-u","user;pass","-s","user;/export;no;no;no;user","-p","-S" ], "image": "dperson/samba", "securityContext":{"privileged":true}, "ports": [{"containerPort": 139, "protocol": "TCP"}, {"containerPort": 445, "protocol": "TCP"}]}]}}}}`
+			overrides := fmt.Sprintf(`{"spec": {"template":  {"spec": {"containers": [{"name": "test-smb1", "command": [ "/sbin/tini", "--", "/usr/bin/samba.sh", "-p","-u","%s;%s","-s","user;/export;no;no;no;user","-p","-S" ], "image": "dperson/samba", "securityContext":{"privileged":true}, "ports": [{"containerPort": 139, "protocol": "TCP"}, {"containerPort": 445, "protocol": "TCP"}]}]}}}}`, username, password)
 			local_k8s_cluster.Kubectl("run", "--overrides", overrides, "--image", "dperson/samba", "test-smb1")
 			Eventually(func() string {
 				podIP = local_k8s_cluster.Kubectl("get", "pods", "-l", "run=test-smb1", "-o", "jsonpath={.items[0].status.podIPs[0].ip}")
@@ -61,14 +65,28 @@ var _ = Describe("Integration", func() {
 			return resp.Status
 		}).Should(Equal("201 Created"))
 
-		Expect(local_k8s_cluster.Kubectl("apply", "-f", "./assets/writer_pod.yaml")).To(ContainSubstring("created"))
+		templ, err := template.New("writer_pod.yaml").ParseFiles("./assets/writer_pod.yaml")
+		Expect(err).NotTo(HaveOccurred())
+		interpolatedWritedPodYaml, err := ioutil.TempFile(os.TempDir(), "writer_pod_yaml")
+		Expect(err).NotTo(HaveOccurred())
+
+		err = templ.Execute(interpolatedWritedPodYaml, struct {
+			ExpectedFileContents string
+			InstanceID           string
+		}{
+			expectedFileContents, instanceID,
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		interpolatedYamlPath := interpolatedWritedPodYaml.Name()
+		Expect(local_k8s_cluster.Kubectl("apply", "-f", interpolatedYamlPath)).To(ContainSubstring("created"))
 		Expect(local_k8s_cluster.Kubectl("apply", "-f", "./assets/reader_pod.yaml")).To(ContainSubstring("created"))
 
-		mountCommand := fmt.Sprintf("mkdir /instance1 && mount -t cifs -o username=user,password=pass //%s/user /instance1 && cat /instance1/foo", podIP)
-		output = local_k8s_cluster.Kubectl("exec", "-n", "eirini", "-i", "integration-test-reader", "bash", "-c", mountCommand)
+		mountCommand := fmt.Sprintf("mkdir /instance1 && mount -t cifs -o username=%s,password=%s //%s/user /instance1 && cat /instance1/foo || true", username, password, podIP)
+		fileReadContents = local_k8s_cluster.Kubectl("exec", "-n", "eirini", "-i", "integration-test-reader", "bash", "-c", mountCommand)
 	})
 
 	It("the file contents written by a pod with a pvc (created by the broker) should be written to the smb share", func() {
-		Expect(output).To(Equal("hi"))
+		Expect(fileReadContents).To(Equal(expectedFileContents))
 	})
 })
