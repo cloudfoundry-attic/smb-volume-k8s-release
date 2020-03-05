3b4893e4dbffc6a8eaf405e37ea6a071ba2d1f3c
Lisa Burns <lcho@pivotal.io>
2020-03-04 16:12:30 -0800

    Add CI task to output go version used by kpack image to a file
    [#171607702](https://www.pivotaltracker.com/story/show/171607702)
    Authored-by: Lisa Burns <lcho@pivotal.io>

---
+++ smb-broker/ci/extract-version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -ex
+
+export PATH=~/go/bin:$PATH
+
+mkdir -p /etc/docker
+cat <<EOF >/etc/docker/daemon.json
+{
+  "storage-driver": "aufs"
+}
+EOF
+
+/usr/local/bin/start-docker &
+
+rc=1
+for i in $(seq 1 100); do
+  echo waiting for docker to come up...
+  set +e
+  docker info
+  rc=$?
+  set -e
+  if [ "$rc" -eq "0" ]; then
+      break
+  fi
+  sleep 1
+done
+
+if [ "$rc" -ne "0" ]; then
+  exit 1
+fi
+
+function kill_docker() {
+    pkill dockerd
+}
+trap kill_docker EXIT
+
+make --directory=smb-volume-k8s-release/smb-broker extract-go-version
+cat /tmp/version
+
+cp /tmp/version /go-version
