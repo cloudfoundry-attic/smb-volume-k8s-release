afb20b8afdf243036bf6bfa5bdb00e6549564adf
Paul Warren <pwarren@pivotal.io>
2020-04-29 12:32:49 -0700

    Add a basic filter that filters out all except MountFailed events
    [#167875644](https://www.pivotaltracker.com/story/show/167875644)

--- log-forwarder/filter/filter.go
+++ log-forwarder/filter/filter.go
@@ -1,1 +1,20 @@
 package filter
+
+import (
+	v1 "k8s.io/api/core/v1"
+)
+
+type Filter struct {
+}
+
+func NewFilter() Filter {
+	return Filter{}
+}
+
+func (filter Filter) Matches(event *v1.Event) (bool) {
+	if event.Reason == "MountFailed" {
+		return true
+	}
+	return false
+}
+
--- log-forwarder/filter/filter_test.go
+++ log-forwarder/filter/filter_test.go
@@ -1,12 +1,47 @@
 package filter_test
 
 import (
+	. "code.cloudfoundry.org/volume-services-log-forwarder/filter"
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
-	//
-	//. "UNKNOWN_PACKAGE_PATH"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
 )
 
 var _ = Describe("Filter", func() {
 
+	var (
+		filter  Filter
+		matches bool
+		event   *v1.Event
+	)
+
+	Describe("#Matches", func() {
+
+		BeforeEach(func() {
+			event = &v1.Event{}
+		})
+
+		JustBeforeEach(func() {
+			filter = NewFilter()
+			matches = filter.Matches(event)
+		})
+
+		Context("given an event that is not MountFailed", func() {
+
+			It("should return false", func() {
+				Expect(matches).To(BeFalse())
+			})
+		})
+
+		Context("given an event that is MountFailed", func() {
+
+			BeforeEach(func() {
+				event.Reason = "MountFailed"
+			})
+
+			It("should return true", func() {
+				Expect(matches).To(BeTrue())
+			})
+		})
+	})
 })
