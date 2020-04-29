fd54ea87d20184f6dd137e567ec146ef8406ee5c
Paul Warren <pwarren@pivotal.io>
2020-04-29 11:11:38 -0700

    Add basic watcher framework to watch for events in the cf-workloads namespace
    [#167875644](https://www.pivotaltracker.com/story/show/167875644)
    
    Authored-by: Paul Warren <pwarren@pivotal.io>

--- log-forwarder/go.mod
+++ log-forwarder/go.mod
@@ -4,6 +4,13 @@ go 1.13
 
 require (
 	code.cloudfoundry.org/smb-volume-k8s-local-cluster v1.0.1-0.20200424134535-28ce3d707459
+	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.9.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	k8s.io/api v0.18.2
+	k8s.io/apimachinery v0.18.2
+	k8s.io/client-go v0.18.2
+	k8s.io/utils v0.0.0-20200414100711-2df71ebbae66 // indirect
 )
--- log-forwarder/main.go
+++ log-forwarder/main.go
@@ -1,9 +1,50 @@
 package main
 
-import "time"
+import (
+	"flag"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/rest"
+	"time"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
 
 func main() {
-	for true {
-		time.Sleep(10 * time.Second)
+	flag.Parse()
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	watchlist := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "events", "cf-workloads",
+		fields.Everything())
+	_, controller := cache.NewInformer(
+		watchlist,
+		&v1.Event{},
+		time.Second * 0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				fmt.Printf("event added: %s \n", obj)
+			},
+			DeleteFunc: func(obj interface{}) {
+				fmt.Printf("event deleted: %s \n", obj)
+			},
+			UpdateFunc:func(oldObj, newObj interface{}) {
+				fmt.Printf("event changed.  Old obj: %s, New obj: %s \n", oldObj, newObj)
+			},
+		},
+	)
+	stop := make(chan struct{})
+	go controller.Run(stop)
+	for{
+		time.Sleep(time.Second)
 	}
 }
